10a3088c40e2144c49b7c8f2cae8bcacb1b883b5
developer@trashcash.ph <sacdalanryanchristian6404@gmail.com>
2022-02-20 21:57:39 +0800

    Firebase setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCBicMMa3UvI0NvlofFDcUBP9X96fKboYo",
+  authDomain: "bcons-app-7fbb3.firebaseapp.com",
+  projectId: "bcons-app-7fbb3",
+  storageBucket: "bcons-app-7fbb3.appspot.com",
+  messagingSenderId: "677774245629",
+  appId: "1:677774245629:web:1ed281fbad8b75b5f70894",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export default app;
