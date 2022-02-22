525c2543e7d755c1ddeaf4e8dd5ca486bbad8b0f
developer@trashcash.ph <sacdalanryanchristian6404@gmail.com>
2022-02-22 11:09:01 +0800

    fix: dashboard display name and remove auth signup component.

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,21 +1,9 @@
 import React, { useState } from "react";
 import Signin from "./signin";
-import Signup from "./signup";
 
 const Auth = () => {
   const [index, setIndex] = useState(false);
-  const toggleIndex = () => {
-    setIndex((prevState) => !prevState);
-  };
-
-  return (
-    <div className="container">
-      {!index ? <Signin /> : <Signup />}
-      <p onClick={toggleIndex}>
-        {!index ? "New User? Click here" : "Have an account?"}
-      </p>
-    </div>
-  );
+  return <div className="container">{!index ? <Signin /> : null}</div>;
 };
 
 export default Auth;
--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -6,7 +6,7 @@ const Dashboard = () => {
   return (
     <div>
       <h1>Dashboard </h1>
-      <h2>Name : {user.displayName}</h2>
+      <h2>Name : {(user.displayName = "BCONS Admin")}</h2>
       <h2>Email : {user.email}</h2>
       <button onClick={logoutUser}>Log out</button>
     </div>
