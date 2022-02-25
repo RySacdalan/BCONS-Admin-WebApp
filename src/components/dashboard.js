e37665a99f7cc469ec9389add80c512685e2a603
developer@trashcash.ph <sacdalanryanchristian6404@gmail.com>
2022-02-25 21:22:22 +0800

    Delete not useful code blocks.

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,18 +1,13 @@
 import React from "react";
-import { useUserContext } from "../context/userContext";
 import Topnavbar from "./topnavbar";
 import "../styles/dashboard.css";
 
 const Dashboard = () => {
-  const { user, logoutUser } = useUserContext();
   return (
     <div className="dashboard">
       <div className="dashboard_wrapper">
         <Topnavbar />
         <h1>Dashboard </h1>
-        <h2>Name : {(user.displayName = "BCONS Admin")}</h2>
-        <h2>Email : {user.email}</h2>
-        <button onClick={logoutUser}>Log out</button>
       </div>
     </div>
   );
