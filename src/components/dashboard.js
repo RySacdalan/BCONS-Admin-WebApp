8c69b773852135753f93efe6d465865730d0ddd9
developer@trashcash.ph <sacdalanryanchristian6404@gmail.com>
2022-02-27 21:20:45 +0800

    Create test  sidenavbar

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Topnavbar from "./topnavbar";
 import "../styles/dashboard.scss";
+import Sidenavbar from "./sidenavbar";
 
 const Dashboard = () => {
   return (
     <div className="dashboard">
       <div className="dashboard_wrapper">
         <Topnavbar />
-        <h1>Dashboard </h1>
+        <div className="sidenav-container">
+          <Sidenavbar />
+          <div className="sample">Web app widgets</div>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/sidenavbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/sidenavbar.scss";
+
+const Sidenavbar = () => {
+  return (
+    <div className="sidenav">
+      <h2>Sidebar</h2>
+    </div>
+  );
+};
+
+export default Sidenavbar;
