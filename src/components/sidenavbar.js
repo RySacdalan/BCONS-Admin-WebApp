7895f2095125572b8dff0d6e7885d0a71b69dd68
developer@trashcash.ph <sacdalanryanchristian6404@gmail.com>
2022-02-28 11:14:55 +0800

    Add list items to sidenavbar.

--- src/components/sidenavbar.js
+++ src/components/sidenavbar.js
@@ -3,8 +3,40 @@ import "../styles/sidenavbar.scss";
 
 const Sidenavbar = () => {
   return (
-    <div className="sidenav">
-      <h2>Sidebar</h2>
+    <div className="sidenavbar">
+      <div className="sidenavbar-wrapper">
+        <div className="sidenavbar-menu">
+          <h3 className="sidenavbar-title">Dashboard</h3>
+          <ul className="sidenavbar-list">
+            <li className="sidenavbar-listItem">
+              <ion-icon name="home"></ion-icon>
+              Home
+            </li>
+            <li className="sidenavbar-listItem">
+              <ion-icon name="people"></ion-icon>
+              Users
+            </li>
+            <li className="sidenavbar-listItem">
+              <ion-icon name="alert-circle"></ion-icon>
+              Reports
+            </li>
+            <li className="sidenavbar-listItem">
+              <ion-icon name="time-outline"></ion-icon>
+              History
+            </li>
+            <div className="lower-listItem">
+              <li className="sidenavbar-listItem">
+                <ion-icon name="settings-outline"></ion-icon>
+                Settings
+              </li>
+              <li className="sidenavbar-listItem">
+                <ion-icon name="information-circle"></ion-icon>
+                About BCONS
+              </li>
+            </div>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
