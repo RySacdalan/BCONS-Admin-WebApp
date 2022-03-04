bc1af67da08442d0bc5200cea69ab0b6d79acdc8
developer@trashcash.ph <sacdalanryanchristian6404@gmail.com>
2022-03-04 21:19:45 +0800

    install react-router-dom version 5

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -3,18 +3,27 @@ import "../styles/dashboard.scss";
 import Topnavbar from "./topnavbar";
 import Sidenavbar from "./sidenavbar";
 import Home from "../pages/home/home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Users from "../pages/users/users";
 
 const Dashboard = () => {
   return (
-    <div className="dashboard">
+    <Router className="dashboard">
       <div className="dashboard_wrapper">
         <Topnavbar />
         <div className="sidenav-container">
           <Sidenavbar />
-          <Home />
+          <Switch>
+            <Route exact path="/home">
+              <Home />
+            </Route>
+            <Route path="/users">
+              <Users />
+            </Route>
+          </Switch>
         </div>
       </div>
-    </div>
+    </Router>
   );
 };
 
---
+++ src/pages/users/users.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./users.scss";
+
+const Users = () => {
+  return (
+    <div className="users">
+      <h3>List of All users</h3>
+    </div>
+  );
+};
+
+export default Users;
