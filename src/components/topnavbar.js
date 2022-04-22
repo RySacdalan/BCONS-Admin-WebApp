168de56a884607b42bbd667dbac83128e9022a6a
rysacdalan <sacdalanryanchristian6404@gmail.com>
2022-04-22 11:51:10 +0800

    Remove darkmode theme.

--- src/components/topnavbar.js
+++ src/components/topnavbar.js
@@ -1,44 +1,21 @@
-import React, { useContext } from "react";
+import React from "react";
 import { useUserContext } from "../context/userContext";
 import "../styles/topnavbar.scss";
 import logo from "../images/BCONS-plain.png";
-import { ThemeContext } from "../context/themeContext";
 import { v4 as uuidv4 } from "uuid";
 
 const Topnavbar = () => {
   const { user } = useUserContext();
-  const [{ theme, isDark }, toggleTheme] = useContext(ThemeContext);
-  console.log(theme);
 
   return (
-    <div
-      className="topnavbar"
-      style={{ backgroundColor: theme.backgroundColor, color: theme.color }}
-      key={uuidv4()}
-    >
+    <div className="topnavbar" key={uuidv4()}>
       <div className="topwrapper" key={uuidv4()}>
         <div className="topleft" key={uuidv4()}>
           <img className="logo" src={logo} alt="BCONS Logo" key={uuidv4()} />
           <h3>| Admin</h3> {/*eslint-disable-line */}
         </div>
         <div className="topright" key={uuidv4()}>
-          <div
-            className="theme-wrapper"
-            style={{ backgroundColor: theme.backgroundColor }}
-            onClick={toggleTheme}
-            key={uuidv4()}
-          >
-            {isDark ? (
-              <ion-icon name="moon"></ion-icon>
-            ) : (
-              <ion-icon name="sunny"></ion-icon>
-            )}
-          </div>
-          <div
-            className="topright_content"
-            style={{ backgroundColor: theme.backgroundColor }}
-            key={uuidv4()}
-          >
+          <div key={uuidv4()}>
             <p>{user.email}</p> {/*eslint-disable-line */}
           </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { ThemeProvider } from "./context/themeContext";
 import { UserContextProvider } from "./context/userContext";
 import { v4 as uuidv4 } from "uuid";
 import { ReportProvider } from "./context/reportContext";
@@ -10,9 +9,7 @@ ReactDOM.render(
   <React.StrictMode key={uuidv4()}>
     <UserContextProvider key={uuidv4()}>
       <ReportProvider key={uuidv4()}>
-        <ThemeProvider key={uuidv4()}>
-          <App key={uuidv4()} />
-        </ThemeProvider>
+        <App key={uuidv4()} />
       </ReportProvider>
     </UserContextProvider>
   </React.StrictMode>,
